91a974124c4b45f518bec2ba23d1006d56f446d2
cheongu <kangsun0528@gmail.com>
2023-01-09 20:48:37 +0900

    no message

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import Layout from "../components/Layout";
+import Main from "../components/Main";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Header />
+      <Main />
+    </Layout>
+  );
+};
+
+export default Home;
