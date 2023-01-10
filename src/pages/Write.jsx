16fcd8bf77b31067f5bb5a60b487ff42abc455d5
cheongu <kangsun0528@gmail.com>
2023-01-10 18:19:29 +0900

    write 기본 추가

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,23 +1,66 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Write = () => {
-  const location = useLocation();
-  console.log("location :>> ", location);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const $ = useSelector((state) => state.todos.$);
+  const [todo, setTodo] = useState({
+    writer: "",
+    title: "",
+    body: "",
+  });
+
+  const onChangeHandler = (event) => {
+    alert("클릭");
+  };
+
   return (
     <div>
-      {/* onChangeHandler */}
-      <h3>작성자</h3>
-      <input />
-      <h3>제목</h3>
-      <input />
-      <h3>내용</h3>
-      <input />
-      <div>
-        {/* onClickHandler */}
-        <button>추가하기</button>
-      </div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (
+            todo.writer.trim() === "" ||
+            todo.title.trim() === "" ||
+            todo.body.trim() === ""
+          ) {
+            return alert("모든 항목을 입력해주세요.");
+          }
+        }}
+      >
+        <label>작성자</label>
+        <input
+          type="text"
+          name="writer"
+          placeholder="작성자의 이름을 입력해주세요. (5자 이내)"
+          value
+          onChange={onChangeHandler}
+          maxLength={5}
+        />
+        <label>제목</label>
+        <input
+          type="text"
+          name="title"
+          placeholder="작성자의 이름을 입력해주세요. (5자 이내)"
+          value
+          onChange={onChangeHandler}
+        />
+        <label>내용</label>
+        <input
+          type="text"
+          name="body"
+          placeholder="내용을 입력해주세요. (200자 이내)"
+          value
+          onChange={onChangeHandler}
+        />
+        <div>
+          <button>추가하기</button>
+        </div>
+      </form>
 
-      <div>{`현재 페이지 : ${location.pathname.slice(1)}`}</div>
       <Link to="/">home 페이지로 이동하기</Link>
     </div>
   );
